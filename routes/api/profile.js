70d58ac98440a8040657f1e694fcd9dffe69e351
Albert Stjärne <albert.stjarne@gmail.com>
2019-04-10 17:26:23 +0200

    adds route to delete user and user profile

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -10,7 +10,7 @@ const validateEducationInput = require('../../validation/education');
 
 // Load profile model
 const Profile = require('../../models/Profile');
-// Load user profile
+// Load user model
 const User = require('../../models/User');
 
 // @route   GET api/profile/test
@@ -262,5 +262,15 @@ router.delete('/education/:edu_id', passport.authenticate('jwt', { session: fals
     .catch(err => res.status(404).json(err));
 });
 
+// @route   DELETE api/profile/
+// @desc    Delete user and profile
+// @access  Private
+router.delete('/', passport.authenticate('jwt', { session: false}), (req, res) => {
+  Profile.findOneAndRemove({ user: req.user.id }).then(() => {
+      User.findOneAndRemove({ _id: req.user.id}).then(() => 
+      res.json({ success: true })
+    );
+    });
+});
 
 module.exports = router;
