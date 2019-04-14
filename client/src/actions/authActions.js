be3cbc824667b806505f5802f3b0b154b8720083
Albert Stjärne <albert.stjarne@gmail.com>
2019-04-14 14:50:52 +0200

    adds login getting user token, adds utils folder and setAuthToken file adding auth token to every request if logged in

--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import setAuthToken from '../utils/setAuthToken';
 import { GET_ERRORS } from './types';
 
 // Register user
@@ -12,4 +13,22 @@ export const registerUser = (userData, history) => dispatch => {
         payload: err.response.data
       })
     );
+};
+
+// Login - get user token
+export const loginUser = (userData) => dispatch => {
+  axios.post('/api/users/login', userData)
+    .then(res => {
+      // Save to localstorage
+      const { token } = res.data;
+      // Set token to localstorage
+      localStorage.setItem('jwtToken', token);
+      // Set token to auth header
+      setAuthToken(token);
+    })
+    .catch(err => 
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data
+      }));
 };
