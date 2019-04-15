c8a08a0ccc8607682d90620189552ecd0079635c
Albert Stjärne <albert.stjarne@gmail.com>
2019-04-15 18:43:04 +0200

    adds professional status with the SelectListGroup component

--- client/src/components/create-profile/CreateProfile.js
+++ client/src/components/create-profile/CreateProfile.js
@@ -43,31 +43,53 @@ class CreateProfile extends Component {
   render() {
     const { errors } = this.state;
 
+    // Select options for status
+    const options = [
+      { label: '* Select Professional Status', value: 0 },
+      { label: 'Developer', value: 'Developer'},
+      { label: 'Junior Developer', value: 'Junior Developer'},
+      { label: 'Senior Developer', value: 'Senior Developer'},
+      { label: 'Manager', value: 'Manager'},
+      { label: 'Student or Learning', value: 'Student or Learning'},
+      { label: 'Instructor or Teacher', value: 'Instructor or Teacher'},
+      { label: 'Intern', value: 'Intern'},
+      { label: 'Other', value: 'Other'},
+    ];
+
     return (
       <div className="create-profile">
-      <div className="container">
-        <div className="row">
-          <div className="col-md-8 m-auto">
-            <h1 className="display-4 text-center">Create Your Profile</h1>
-            <p className="lead text-center">
-              Let's get some information to make your profile stand out
-            </p>
-            <small className="d-block pb-3">* = required fields</small>
-            <form onSubmit={this.onSubmit}>
-              <TextFieldGroup 
-                placeholder="* Profile Handle"
-                name="handle"
-                value={this.state.handle}
-                onChange={this.onChange}
-                error={errors.handle}
-                info="A unique handler for your profile URL. Your full name, company name, nickname."
-              />
-            </form>
+        <div className="container">
+          <div className="row">
+            <div className="col-md-8 m-auto">
+              <h1 className="display-4 text-center">Create Your Profile</h1>
+              <p className="lead text-center">
+                Let's get some information to make your profile stand out
+              </p>
+              <small className="d-block pb-3">* = required fields</small>
+              <form onSubmit={this.onSubmit}>
+                <TextFieldGroup 
+                  placeholder="* Profile Handle"
+                  name="handle"
+                  value={this.state.handle}
+                  onChange={this.onChange}
+                  error={errors.handle}
+                  info="A unique handler for your profile URL. Your full name, company name, nickname."
+                />
+                <SelectListGroup 
+                  placeholder="Status"
+                  name="status"
+                  value={this.state.status}
+                  onChange={this.onChange}
+                  options={options}
+                  error={errors.status}
+                  info="Give us an idea of where you are at in your career"
+                />
+
+              </form>
+            </div>
           </div>
         </div>
-      </div>
-        
-      </div>
+    </div>
     )
   }
 }
