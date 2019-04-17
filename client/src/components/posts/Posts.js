5eb72fe14851192c9722cf92d4c2053f5a43202b
Albert Stjärne <albert.stjarne@gmail.com>
2019-04-17 22:10:36 +0200

    adds PostItem and PostFeed component files, mapping over the posts, adding the action to get posts from the db, rendering posts and spinner while loading

--- client/src/components/posts/Posts.js
+++ client/src/components/posts/Posts.js
@@ -3,9 +3,24 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import PostForm from './PostForm';
 import Spinner from '../common/Spinner';
+import { getPosts } from '../../actions/postActions';
+import PostFeed from './PostFeed';
 
 class Posts extends Component {
+  componentDidMount() {
+    this.props.getPosts();
+  }
+
   render() {
+    const { posts, loading } = this.props.post;
+    let postContent;
+
+    if(posts === null || loading) {
+      postContent = <Spinner />;
+    } else {
+      postContent = <PostFeed posts={posts} />;
+    }
+
     return (
       <div className="feed">
         <div className="container">
@@ -18,4 +33,13 @@ class Posts extends Component {
   }
 }
 
-export default Posts;
+Posts.propTypes = {
+  post: PropTypes.object.isRequired,
+  getPosts: PropTypes.func.isRequired,
+}
+
+const mapStateToProps = state => ({
+  post: state.post
+});
+
+export default connect(mapStateToProps, { getPosts })(Posts);
