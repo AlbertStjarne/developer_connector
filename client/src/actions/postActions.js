db271171a65538b1ed8277ddb128ef45626c6241
Albert Stjärne <albert.stjarne@gmail.com>
2019-04-18 13:34:24 +0200

    adds deletePost functionality

--- client/src/actions/postActions.js
+++ client/src/actions/postActions.js
@@ -4,7 +4,8 @@ import {
   ADD_POST,
   GET_ERRORS,
   GET_POSTS,
-  POST_LOADING
+  POST_LOADING,
+  DELETE_POST
 } from './types';
 
 // Add post
@@ -40,6 +41,24 @@ export const getPosts = postData => dispatch => {
       dispatch({
         type: GET_POSTS,
         payload: null
+      })
+    );
+};
+
+// Delete post
+export const deletePost = id => dispatch => {
+  axios
+    .delete(`/api/posts/${id}`)
+    .then(res => 
+      dispatch({
+        type: DELETE_POST,
+        payload: id
+      })
+    )
+    .catch(err => 
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data
       })
     );
 };
